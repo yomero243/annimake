7fd2a043cb953f3850c7ea157fa1c5142c5c7106
Garbriel Cerdio <yo_mero_yo@hotmail.com>
2026-01-27 21:03:07 -0600

    Eliminadas configuraciones específicas de Next.js para exportación, ruta base y optimización de imágenes.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/annimake',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
