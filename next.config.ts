3b60d3cac5ef29e21e0a61b5c870cae308dc5e75
Gabriel Cerdio <112841951+yomero243@users.noreply.github.com>
2026-01-27 21:00:17 -0600

    clean code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/annimake',
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
