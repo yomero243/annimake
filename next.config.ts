63cae6994e906a39053b4583d40521561acb9b7b
Garbriel Cerdio <yo_mero_yo@hotmail.com>
2026-01-27 21:07:17 -0600

    erase

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/annimake',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
